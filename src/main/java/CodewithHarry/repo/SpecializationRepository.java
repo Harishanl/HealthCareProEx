1affb62c82ff42c680cbb9fdaf820f1c4efee407
Harishanl <harishankar20k01@gmail.com>
2025-06-07 19:31:26 +0530

    spec repo update

---
+++ src/main/java/CodewithHarry/repo/SpecializationRepository.java
@@ -0,0 +1,9 @@
+package CodewithHarry.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import CodewithHarry.entites.Specialization;
+
+public interface SpecializationRepository extends JpaRepository<Specialization, Long> {
+
+}
