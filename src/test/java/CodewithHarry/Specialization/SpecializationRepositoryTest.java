a2c88621ed7ce24fc6819de58c20de0d2ab9c94c
Harishanl <harishankar20k01@gmail.com>
2025-06-08 00:16:39 +0530

    spec Repo and Test case

---
+++ src/test/java/CodewithHarry/Specialization/SpecializationRepositoryTest.java
@@ -0,0 +1,62 @@
+package CodewithHarry.Specialization;
+
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.List;
+
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import CodewithHarry.entites.Specialization;
+import CodewithHarry.repo.SpecializationRepository;
+
+@DataJpaTest(showSql = true)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+@TestMethodOrder(OrderAnnotation.class)
+public class SpecializationRepositoryTest {
+	
+	@Autowired
+	private SpecializationRepository repo;
+	
+	//1.Test save operation
+	@Test
+	@Order(1)
+	public void testSpecCreate()
+	{
+		Specialization	spec= new Specialization(null, "CRDLS", "Cardiologists", "They’re experts on the heart and blood vessels");
+		spec= repo.save(spec);
+		assertNotNull(spec.getId(),"spec is Not Created!");
+		
+		
+	}
+	
+	//2. Test display all operation
+
+	@Test
+	@Order(2)
+	public void testSpecFetchAll() {
+		List<Specialization> list = repo.findAll();
+		assertNotNull(list);
+		if(list.isEmpty()) {
+			fail("No data exit in DataBase!");
+		}
+		
+	}
+	
+	
+	
+	
+
+}
